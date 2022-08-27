31321d852efac8dd22d770e14bd40ba1b0511b3b
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-08-27 11:45:29 -0700

    Added branching with different edges cases. It will allow for the answer to changed based on user input.

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,27 @@ function getSetSurvey(event)  {
   const question3 = document.querySelector("input[name='milkFirst']:checked").value;
   const question4 = document.querySelector("input[name='devType']:checked").value;
   const question5 = document.querySelector("input[name='pets']:checked").value;
-  console.log(question1);
-  console.log(question2);
+
+  let language;
+
+  if (question4 === "frontEnd")  {
+    language = "Python";
+  } else if (question4 === "backEnd")  {
+    language = "Assembly";
+  } else if (question4 === "backEnd" && question1 === "Blue" || question5 === "Dogs")  {
+    language = "C++";
+  } else if (question4 === "frontEnd" && question2 === "yes")  {  
+    language = "C++";
+  } else if (question3 === "cereal")  {
+    language = "Assembly";
+  } else if (question2 === "no")  {
+    language = "Python";
+  } else  {
+    language = "Stop messing around. Input something real"
+  }
+  console.log(language);
+  document.getElementById("output").innerText = language;
+
 };
 
 window.addEventListener("load", function()  {
