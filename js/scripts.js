cc6fe525b056b755470c48ae251379b15306987d
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-08-27 10:46:33 -0700

    Added .values to question variables and a type in name. It will allow the the values to be stored correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 function getSetSurvey(event)  {
   event.preventDefault();
-  const question1 = document.querySelector("input[name='colorFave']:checked");
-  const question2 = document.querySelector("input[name='birds']:checked");
-  const question3 = document.querySelector("input[name='milkFirst']:checked");
-  const question4 = document.querySelector("input[name='devType']:checked");
-  const question5 = document.querySelector("input[name='pets']:checked");
+  const question1 = document.querySelector("input[name='colorFav']:checked").value;
+  const question2 = document.querySelector("input[name='birds']:checked").value;
+  const question3 = document.querySelector("input[name='milkFirst']:checked").value;
+  const question4 = document.querySelector("input[name='devType']:checked").value;
+  const question5 = document.querySelector("input[name='pets']:checked").value;
   console.log(question1);
-
+  console.log(question2);
 };
+
+window.addEventListener("load", function()  {
+  const form = document.getElementById("survey");
+  form.addEventListener("submit",getSetSurvey);
+});
